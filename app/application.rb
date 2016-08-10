605e491c8d2764f610cddf1f3dbbc757562aebcd
jaspercurry <jaspercurry@gmail.com>
2016-08-10 16:52:13 -0400

    made time greeter using rack

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.now.hour < 12
+      resp.write "Good Morning!\n"
+      else
+        resp.write "Good Afternoon!\n"
+      end
+    resp.finish
+  end
+
+end
